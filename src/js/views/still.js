2e8e68a07cc5d1a7eaeb54b55a5892ca2f27d31f
tiarra11 <tmcin0329@gmail.com>
2019-06-03 17:42:10 +0000

    changes

--- src/js/views/still.js
+++ src/js/views/still.js
@@ -4,6 +4,12 @@ export class Still extends React.Component {
 	render() {
 		return (
 			<div className="container bg-transparent mt-5">
+				<img
+					src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDoteB8PiLzD6iehnMXHJsAH9aMIoxw2VTn0Zm_t4mEe8z6Hy5Eg"
+					className="rounded float-right imgshadow"
+					alt="..."
+				/>
+
 				<div className="row">
 					<div className="col-md-12 text-center still">sTill</div>
 
@@ -13,6 +19,7 @@ export class Still extends React.Component {
 				<div className="row text d-flex justify-content-center">
 					<div className="text">Digital</div>
 				</div>
+
 				<div className="row text d-flex justify-content-center ml-7">
 					<div className="col-2" />
 					<div className="col-3">Strategy</div>
