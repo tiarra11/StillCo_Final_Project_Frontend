6a8a50cc4a8cbc5cc8ba67f188c28539b3802241
ProspersMartin <martinprospers1@gmail.com>
2019-06-11 14:27:23 -0400

    changed functionality/effects to footer and navbar along with styling for about / landing page

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,17 +3,24 @@ import { Link } from "react-router-dom";
 
 export class Navbar extends React.Component {
 	render() {
+		let path = window.location.pathname;
 		return (
-			<nav className="hide navbar navbar-expand-md navbar-light">
-				<div className="navbar-collapse collapse" id="collapsingNavbar">
+			<nav className="navbar navbar-expand-md navbar-light">
+				<div
+					className={path == "/" ? "hide navbar-collapse collapse" : "navbar-collapse collapse"}
+					id="collapsingNavbar">
 					<ul className="navbar-nav">
 						<li className="nav-item mr-4">
-							<Link to="/" className="stillLogNav navcolor">
-								s<span className="stillTNav">T</span>
-								ill
+							<Link to="/" className="stillLogoNav navcolor">
+								<div className="unHover">
+									<span className="sPositioningNav">s</span>
+									<span className="stillTNav">T</span>
+									<span className="illpositioningNav">ill</span>
+								</div>
 							</Link>
 						</li>
 					</ul>
+					{path}
 					<ul className="navbar-nav ml-auto">
 						<li className="nav-item mr-4">
 							<Link to="/portfolio">Portfolio</Link>
