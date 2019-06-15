bc8e3cd7de198b7cfde3acde4fb58ce4facf3e37
ProspersMartin <martinprospers1@gmail.com>
2019-06-15 13:14:13 -0400

    added styling to scss and aboutStill page

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -67,20 +67,7 @@ const getState = ({ getStore, setStore }) => {
 				}
 			]
 		},
-		actions: {
-			// changeColor: (index, color) => {
-			// 	//get the store
-			// 	const store = getStore();
-			// 	//we have to loop the entire demo array to look for the respective index
-			// 	//and change its color
-			// 	const demo = store.demo.map((elm, i) => {
-			// 		if (i === index) elm.background = color;
-			// 		return elm;
-			// 	});
-			// 	//reset the global store
-			// 	setStore({ demo: demo });
-			// }
-		}
+		actions: {}
 	};
 };
 
