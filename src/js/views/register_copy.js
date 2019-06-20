3f160b27e95cd2e4af1c99290c797defe531e2a6
tiarra11 <tmcin0329@gmail.com>
2019-06-20 13:43:12 +0000

    thu 9:42

--- src/js/views/register_copy.js
+++ src/js/views/register_copy.js
@@ -15,24 +15,37 @@ export class Register extends React.Component {
 				{({ store, actions }) => {
 					return (
 						<div>
-							<div className="text-center py-5">
-								<h1>Join the Collective</h1>
-							</div>
 							<div className="d-flex justify-content-center">
+								<div className="text-center py-5">
+									<h1>Join the Collective</h1>
+								</div>
 								<Form>
 									<Form.Group as={Row} controlId="formHorizontalName">
-										<Col>
-											<Form.Control type="formHorizontalName" placeholder="Name" />
+										<Col sm={75}>
+											<Form.Control type="name" placeholder="Name" />
 										</Col>
 									</Form.Group>
 									<Form.Group as={Row} controlId="formHorizontalEmail">
-										<Col>
+										<Col sm={75}>
 											<Form.Control type="email" placeholder="Email" />
 										</Col>
 									</Form.Group>
 									<Form.Group as={Row} controlId="formHorizontalPassword">
-										<Col>
+										<Col lg={75}>
 											<Form.Control type="password" placeholder="Password" />
+										</Col>
+									</Form.Group>
+									<Form.Group as={Row}>
+										<Col sm={{ span: 20, offset: 3 }}>
+											<Button
+												onClick={() => {
+													let name = document.querySelector("[type=name]").value;
+													let email = document.querySelector("[type=email]").value;
+													let password = document.querySelector("[type=password]").value;
+													actions.createClient(name, email, password);
+												}}>
+												Create Account
+											</Button>
 										</Col>
 									</Form.Group>
 									<Form.Group as={Row}>
